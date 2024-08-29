64f2d792972d4c1f0b32aa05147275c71dd907b5
paad <paadfaraji@gmail.com>
2024-08-29 12:00:56 +0330

    adding product model implament Repositories

--- Bulky.DataAccess/Repository/UnitOfWork.cs
+++ Bulky.DataAccess/Repository/UnitOfWork.cs
@@ -11,15 +11,14 @@ namespace Bulky.DataAccess.Repository
     public class UnitOfWork : IUnitOfWork
     {
         public ICategoryRepository Category { get; private set; }
+        public IProductRepository Product { get; private set; }
         private ApplicationDbContext _context;
         public UnitOfWork(ApplicationDbContext context) 
         {
             _context = context;
-            Category= new CategoryRepository(context);
+            Category= new CategoryRepository(_context);
+            Product= new ProductRepository(_context);
         }
-
-      
-
         public void Save()
         {
             _context.SaveChanges();
