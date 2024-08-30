fe51e31a39c9a9fedb4df0e0555d01524ec25c7f
paad <paadfaraji@gmail.com>
2024-08-30 14:14:09 +0330

    adding text area and merge edit and create togheter  and made Upsert inside them

--- Bulky.DataAccess/Data/ApplicationDbContext.cs
+++ Bulky.DataAccess/Data/ApplicationDbContext.cs
@@ -46,7 +46,9 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 99,
                     Price = 90,
                     Price50 = 85,
-                    Price100 = 80
+                    Price100 = 80,
+                    CategoryId=5,
+                    ImageUrl=""
                 },
                 new Product
                 {
@@ -58,7 +60,9 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 40,
                     Price = 30,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId=3,
+                    ImageUrl=""
                 },
                 new Product
                 {
@@ -70,7 +74,9 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 55,
                     Price = 50,
                     Price50 = 40,
-                    Price100 = 35
+                    Price100 = 35,
+                    CategoryId=4,
+                    ImageUrl = ""
                 },
                 new Product
                 {
@@ -82,7 +88,9 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 70,
                     Price = 65,
                     Price50 = 60,
-                    Price100 = 55
+                    Price100 = 55,
+                    CategoryId=5,
+                    ImageUrl = ""
                 },
                 new Product
                 {
@@ -94,7 +102,9 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 30,
                     Price = 27,
                     Price50 = 25,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId=4,
+                    ImageUrl = ""
                 },
                 new Product
                 {
@@ -106,7 +116,9 @@ namespace Bulky.DataAccess.Data
                     ListPrice = 25,
                     Price = 23,
                     Price50 = 22,
-                    Price100 = 20
+                    Price100 = 20,
+                    CategoryId=3,
+                    ImageUrl = ""
                 }
                 );
 
--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -1,7 +1,9 @@
 ﻿using Bulky.DataAccess.Data;
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Bulky.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 namespace BulkyBookWeb.Areas.Admin.Controllers
 {
     [Area("Admin")]
@@ -19,55 +21,86 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
         public IActionResult Index()
         {
             List<Product> objProductList = _UnitOfWork.Product.GetAll().ToList();
-
             return View(objProductList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
-
-            return View();
+            IEnumerable<SelectListItem> CategoryList = _UnitOfWork.Category.GetAll().Select(
+                u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }
+                );
+            ProductViewModel productviewmodel = new()
+            {
+                CategoryList = CategoryList,
+                Product = new Product()
+            };
+            if(id==null || id == 0)
+            {
+                return View(productviewmodel);
+            }
+            else
+            {
+                productviewmodel.Product=_UnitOfWork.Product.Get(u=>u.Id==id);
+                return View(productviewmodel);
+            }
+            
         }
         [HttpPost]
-        public IActionResult Create(Product obj)
+        public IActionResult Upsert(ProductViewModel obj,IFormFile? File )
         {
             if (ModelState.IsValid)
             {
-                _UnitOfWork.Product.Add(obj);
+                _UnitOfWork.Product.Add(obj.Product);
                 _UnitOfWork.Save();
                 TempData["Success"] = "Product Created SuccessFully ";
                 return RedirectToAction("Index");
             }
-            return View();
-        }
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
+            else
             {
-                return NotFound();
-            }
-            Product? prodedit = _UnitOfWork.Product.Get(i => i.Id == id);
-            if (prodedit == null)
-            {
-                return NotFound();
-            }
+                obj.CategoryList = _UnitOfWork.Category.GetAll().Select(
+                u => new SelectListItem
+                 {
+                     Text = u.Name,
+                    Value = u.Id.ToString()
+                }
+                );
 
-            return View(prodedit);
+                return View(obj.CategoryList);
+            }
+            //return View();
         }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null || id == 0)
+        //    {
+        //        return NotFound();
+        //    }
+        //    Product? prodedit = _UnitOfWork.Product.Get(i => i.Id == id);
+        //    if (prodedit == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(prodedit);
+        //}
+        //[HttpPost]
+        //public IActionResult Edit(Product obj)
+        //{
 
 
-            if (ModelState.IsValid)
-            {
-                _UnitOfWork.Product.Update(obj);
-                _UnitOfWork.Save();
-                TempData["Success"] = "Product Update SuccessFully ";
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
+        //    if (ModelState.IsValid)
+        //    {
+        //        _UnitOfWork.Product.Update(obj);
+        //        _UnitOfWork.Save();
+        //        TempData["Success"] = "Product Update SuccessFully ";
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
